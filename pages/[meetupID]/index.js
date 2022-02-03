2413cd4b320b9e532e86ce0ea888a4289da2dfbc
Stephen <omalleypatstephen@gmail.com>
2022-02-03 14:05:22 +0000

    https://youtu.be/MFuwkrseXVE?t=8092

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -1,18 +1,47 @@
-import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
     return(
-        <Fragment>
-            <img
-                src = 'https://wallsdesk.com/wp-content/uploads/2017/01/Monkey-Photos.jpg'
-                alt = 'A First Meetup'
-            />
-            <h1>A First Meetup</h1>#
-            <address>Street, City</address>
-            <p>The Description</p>
-        </Fragment>
+        <MeetupDetail image = "https://img.xcitefun.net/users/2015/01/382645,xcitefun-philippines-tour-6.jpg" title = "Meetup One" address = "Some Place" description = "First" />
     );
-    
+}
+
+export async function getStaticPaths() {
+    return{
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupID: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupID: 'm2',
+                },
+            },
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupID = context.params.meetupID;
+
+    console.log(meetupID);
+
+    return{
+        props: {
+            meetups: {
+                image: "https://img.xcitefun.net/users/2015/01/382645,xcitefun-philippines-tour-6.jpg",
+                id: meetupID,
+                title: "Meetup One" ,
+                address: "Some Place" ,
+                description: "First"
+            }
+        }
+    }
 }
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-https://youtu.be/MFuwkrseXVE?t=4794import MeetupList from '../components/meetups/MeetupList';
+//https://youtu.be/MFuwkrseXVE?t=4794
+
+//https://youtu.be/MFuwkrseXVE?t=8092
+import { useEffect, useState } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
 
 const DUMMY = [
     {
@@ -19,9 +24,31 @@ const DUMMY = [
     }
 ]
 
-function HomePage() {
-    return <MeetupList meetups = {DUMMY} />
+function HomePage(props) {
+    //"export async function getStaticProps()" replaces and provides the 
+    // same fuction of the commented code below, but instead it does it 
+    // on the server side compaired to the clients side.
+
+    //const [loadedMeetups, setLoadedMeetups] = useState([]);
+    //useEffect(() => {
+        // Send a http request and fetch data
+    //    setLoadedMeetups(DUMMY);
+    //}, []);
+
+    return <MeetupList meetups = {props.meetups} />
     
+}
+
+export async function getStaticProps() {
+    // fetch data from an API or Database
+    return {
+        props: {
+            meetups: DUMMY
+        },
+        //Reloads the server web page after however many seconds if their
+        // is data update requests
+        revalidate: 10 // 10 is time in seconds
+    };
 }
 
 export default HomePage;
